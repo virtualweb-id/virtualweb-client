79a2547c6ff0ff7184bb2f01752246ca97d03490
muhammadhaikal <muh.heykall@gmail.com>
2021-02-19 20:32:25 +0700

    resolve bug

--- src/views/index.js
+++ src/views/index.js
@@ -1,2 +1,4 @@
 export { default as Home } from './Home'
 export { default as Undanganku } from './Undanganku'
+export { default as Register } from './Register'
+export { default as Login } from './Login'
