a94f1a6e563e5d551844b0257ad859a326e1718f
raflirach <rafli060395@gmail.com>
2021-02-20 08:33:09 +0700

    add: use effect

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { Navbar, Dropdown } from '../components'
 import { LandingHero, Content } from '../components'
 
 
 const Home = () => {
+  const { name } = useSelector(state => state.user)
   const [dropdown, setDropdown] = useState(false)
+  const history = useHistory()
 
   const toggle = () => {
     setDropdown(!dropdown)
   }
+
+  useEffect(() => {
+    if(name) history.push('/undanganku')
+  }, [])
 
   useEffect(() => {
     const hidedropdown = () => {
