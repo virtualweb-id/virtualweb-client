4e884d8e6b2fb446a1fd7f768a41771eb9b265da
amandarzq <amanda.rzq@gmail.com>
2021-02-21 03:20:54 +0700

    scrollable invitation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,23 +18,23 @@ const Sidebar = () => {
               <svg className="w-6 h-6 stroke-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
               </svg>
-              <Link className={ pathname === `/dashboard` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard">Dashboard</Link>
+              <Link className={ pathname === `/dashboard` ? "p-4 mr-4 text-blue-400" : "p-4 mr-4"} to="/dashboard">Dashboard</Link>
             </a>
             <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
             <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" /></svg>
-              <Link className={ pathname === `/dashboard/wedding` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard/wedding">Wedding Information</Link>
+              <Link className={ pathname === `/dashboard/wedding` ? "p-4 mr-4 text-blue-400" : "p-4 mr-4"} to="/dashboard/wedding">Wedding Information</Link>
             </a>
             <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 3.5a1.5 1.5 0 013 0V4a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-.5a1.5 1.5 0 000 3h.5a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-.5a1.5 1.5 0 00-3 0v.5a1 1 0 01-1 1H6a1 1 0 01-1-1v-3a1 1 0 00-1-1h-.5a1.5 1.5 0 010-3H4a1 1 0 001-1V6a1 1 0 011-1h3a1 1 0 001-1v-.5z" /></svg>
-              <Link className={ pathname === `/dashboard/invitation` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard/invitation">Invitation</Link>
+              <Link className={ pathname === `/dashboard/invitation` ? "p-4 mr-4 text-blue-400" : "p-4 mr-4"} to="/dashboard/invitation">Invitation</Link>
             </a>
             <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" /></svg>
-              <Link className={ pathname === `/dashboard/guests` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard/guests">Guests</Link>
+              <Link className={ pathname === `/dashboard/guests` ? "p-4 mr-4 text-blue-400" : "p-4 mr-4"} to="/dashboard/guests">Guests</Link>
             </a>
             <a className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300" href="#">
             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2.94 6.412A2 2 0 002 8.108V16a2 2 0 002 2h12a2 2 0 002-2V8.108a2 2 0 00-.94-1.696l-6-3.75a2 2 0 00-2.12 0l-6 3.75zm2.615 2.423a1 1 0 10-1.11 1.664l5 3.333a1 1 0 001.11 0l5-3.333a1 1 0 00-1.11-1.664L10 11.798 5.555 8.835z" clipRule="evenodd" /></svg>
-              <Link className={ pathname === `/dashboard/send` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard/send">Send Invitation</Link>
+              <Link className={ pathname === `/dashboard/send` ? "p-4 mr-4 text-blue-400" : "p-4 mr-4"} to="/dashboard/send">Send Invitation</Link>
             </a>
           </div>
         </div>
