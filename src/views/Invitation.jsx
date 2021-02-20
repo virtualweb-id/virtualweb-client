461249b96c23bb783705e1f580c1244564516d23
amandarzq <amanda.rzq@gmail.com>
2021-02-21 01:44:10 +0700

    Edited hero on invitation

--- src/views/Invitation.jsx
+++ src/views/Invitation.jsx
@@ -54,47 +54,9 @@ const Invitation = ({ hours, minutes, seconds }) => {
             </h1>
             <p style={{ color: holder.textColor }}>{wedding.date}</p>
         </div>
-        <div
-          className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
-          style={{ height: "70px", transform: "translateZ(0)" }}
-        >
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              className="text-gray-300 fill-current"
-              points="2560 0 2560 100 0 100"
-            ></polygon>
-          </svg>
-        </div>
       </div>
 
       <section className="relative py-20">
-        <div
-          className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-          style={{ height: "80px", transform: "translateZ(0)" }}
-        >
-          <svg
-            className="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon
-              className="text-white fill-current"
-              points="2560 0 2560 100 0 100"
-            ></polygon>
-          </svg>
-        </div>
             {/* End of hero section */}
          <div className="body-font"
           style={{ color: holder.backgroundColor }}>
