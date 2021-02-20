5ef1c1479ddfb4345e37ea2eb8efea3bac570ccc
raflirach <rafli060395@gmail.com>
2021-02-20 11:03:44 +0700

    edit: Link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Link, useRouteMatch } from 'react-router-dom'
+import { Link, useLocation, useRouteMatch } from 'react-router-dom'
 
 const Sidebar = () => {
   const {path, url} = useRouteMatch()
+  const { pathname } = useLocation()
   return (
     <>
       <div className="h-full flex flex-col w-1/6 bg-gray-200 p-5 cursor-pointer">
-          <Link to="/">UNDANGANKU</Link>
-            <Link className={ url === `/undanganku` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/undanganku">Dashboard</Link>
-            <Link className={ url === `/myinvitation` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/myinvitation">Invitation</Link>
-            <Link className={ url === `/myguests` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/myguests">Guests</Link>
-            <Link className={ url === `/sendinvitations` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/sendinvitations">Send Invitation</Link>
+          Undanganku
+            <Link className={ pathname === `/dashboard` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard">Dashboard</Link>
+            <Link className={ pathname === `/dashboard/invitation` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard/invitation">Invitation</Link>
+            <Link className={ pathname === `/dashboard/guests` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard/guests">Guests</Link>
+            <Link className={ pathname === `/dashboard/send` ? "p-4 mr-4 text-red-400" : "p-4 mr-4"} to="/dashboard/send">Send Invitation</Link>
         </div>
     </>
   )
