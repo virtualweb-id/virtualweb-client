6c28a939e485d14520f79041f28ad17b5cc9b302
amandarzq <amanda.rzq@gmail.com>
2021-02-22 04:36:31 +0700

    landing page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import Fade from 'react-reveal/Fade';
+import build from '../assets/build-team.json'
+import Lottie from "lottie-react"
 
 const Entertainme = () => {
   const history = useHistory()
@@ -10,17 +13,28 @@ const Entertainme = () => {
 
     return (
       <>
-      <div className=" font-sans flex justify-center items-center flex-col mb-5" id="content"
-      style={{width: '100vw', height: '100vh'}} >
-        <div className="flex justify-center items-center">
-          <div className=" p-5 m-5 bg-blue-200">TEMA 1</div>
-          <div className="p-5 m-5 bg-blue-200">TEMA 2</div>
-          <div className=" p-5 m-5 bg-blue-200">TEMA 3</div>
+      <div className=" flex justify-center items-center md:flex-row flex-col mb-5 poppins p-10 md:p-20 mt-10" id="content"
+      style={{width: '100%', height: '100%'}} >
+        {/* Left */}
+        <div className="md:w-1/2 font-bold">
+          <Fade left>
+            <p className=" md:text-4xl text-green-600 text-2xl text-extrabold">UNDANGANKU</p>
+            <p className="text-gray-500 font-extrabold leading-relaxed my-3">Undanganku helps you make your own customized wedding invitation with few simple clicks. Just by signing in, you can have access to our invitation builder and start designing your dream invitation! Invite your friends and family and share your precious moments with your loved ones, fast and stress-free.</p>
+              <button className="md:text-xl md:px-5 px-2 flex justify-center block font-bold text-gray-600 bg-green-400 w-1/3 md:py-3 py-1 text-lg mt-5 rounded-lg
+              bg-white hover:text-gray-700 hover:bg-green-500 flex items-center"
+              onClick={() => redirectDashboard()}>Ke dashboard
+            </button>
+          </Fade>
         </div>
-        <button className="md:text-xl md:px-10 px-3 block font-bold text-white bg-blue-300 w-1/3 md:py-3 py-1 text-lg mt-5
-          bg-white hover:text-gray-900 flex items-center"
-          onClick={() => redirectDashboard()}>Ke dashboard
-        </button>
+        {/* Right */}
+        <div className="md:w-1/2 font-bold">
+          <Fade right>
+            <Lottie animationData={build} 
+              loop 
+              autoPlay 
+              className="m-10"/>
+          </Fade>
+        </div>
       </div>
       </>
     )
