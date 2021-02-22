c6023c6a5975df63cbb579893318e6ea5313e1c2
amandarzq <amanda.rzq@gmail.com>
2021-02-22 14:25:55 +0700

    added lazy load image package

--- src/components/OurService.jsx
+++ src/components/OurService.jsx
@@ -1,35 +1,33 @@
 import React from 'react'
-import { useHistory } from 'react-router-dom'
 import Fade from 'react-reveal/Fade';
 import build from '../assets/build-team.json'
 import Lottie from "lottie-react"
 import imageHolder from '../assets/swirl.jpg'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const OurService = () => {
-  const history = useHistory()
-
-  const redirectDashboard = () => {
-    if(localStorage.access_token) history.push('/dashboard')
-    history.push('/login')
-  }
-
     return (
       <>
-      <div className=" flex justify-center items-center md:flex-row flex-col poppins bg-red-300 relative"
+      <div className=" flex justify-center items-center md:flex-row h-full flex-col poppins bg-red-300 relative"
         style={{width: '100%', height: '80%'}} >
           {/* Left */}
-          <div className="md:w-1/2 font-bold sticky top-0 backimg" style={{zIndex: '1'}}>
-            <img src={imageHolder} alt="wedding image"/>
+          <div className="md:w-1/2 font-bold top-0 backimg" style={{ maxHeight: '90%'}}>
+          <LazyLoadImage
+            alt="happy bride"
+            src={imageHolder} />
           </div>
           {/* Right */}
           <div className="md:w-1/2 font-bold ">
             <h1 className="text-white text-center text-3xl">Our Services</h1>
-          <ul>
-            <li>Invitation builder</li>
-            <li>Personalized invitation page</li>
-            <li>Countdown before day of event</li>
-            <li>Digital wedding gift money feature</li>
-          </ul>
+            <div className="flex flex-col justify-center items-center">
+              <ul className="leading-loose my-10">
+                <li>Invitation builder</li>
+                <li>Personalized invitation page</li>
+                <li>Countdown until the event</li>
+                <li>Digital wedding gift feature</li>
+                <li>Guest comment</li>
+              </ul>
+            </div>
           </div>
         </div> 
       </>
