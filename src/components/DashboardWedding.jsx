710f978716e52e630e80414a59865c059ac9c838
amandarzq <amanda.rzq@gmail.com>
2021-02-23 01:28:50 +0700

    Edited wedding dashboard

--- src/components/DashboardWedding.jsx
+++ src/components/DashboardWedding.jsx
@@ -9,42 +9,8 @@ const DashboardWedding = () => {
   const dispatch = useDispatch()
   const { wedding, isLoading } = useSelector(state => state.wedding)
 
-  // const [userWedding, setUserWedding] = useState({
-  //   id: 0,
-  //   WeddingId: 0,
-  //   title: '' ,
-  //   date: '',
-  //   address: '',
-  //   groomName: '',
-  //   brideName: '',
-  //   groomImg: '',
-  //   brideImg: '',
-  //   status: false,
-  // })
-  // console.log(userWedding, 'ini dari dashboard')
-
-  // useEffect(() => {
-  //   setUserWedding({
-  //     id: wedding.id,
-  //     WeddingId: wedding.WeddingId,
-  //     title: wedding.title,
-  //     date: wedding.date.slice(0, 10),
-  //     address: wedding.address,
-  //     groomName: wedding.groomName,
-  //     brideName: wedding.brideName,
-  //     groomImg: wedding.groomImg,
-  //     brideImg: wedding.brideImg,
-  //     status: wedding.status,
-  //   })
-  // }, [wedding])
-
-  // const [previewSourceGroom, setPreviewResultGroom] = useState('')
-  // const [previewSourceBride, setPreviewResultBride] = useState('')
-
   const onChange = (e) => {
     let { name, value } = e.target
-    // const newInput = { ...userWedding, [name]: value }
-    // setUserWedding(newInput)
     dispatch(changeWeddingState(name, value))
   }
 
@@ -61,11 +27,6 @@ const DashboardWedding = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault()
-    // const { id, WeddingId, title, date, address, groomName, brideName } = userWedding
-    // const brideImg = previewSourceBride
-    // const groomImg = previewSourceGroom
-    // const userWedding = { id, WeddingId, title, date, address, groomName, brideName, brideImg, groomImg }
-    // console.log(userWedding)
     createBtnLoading('weddingBtn', 'Saving..')
     await dispatch(updateWedding())
     cancelBtnLoading('weddingBtn', 'SUBMIT')
@@ -74,84 +35,76 @@ const DashboardWedding = () => {
   if(isLoading) return <h1>Loading..</h1>
   return (
     <>
-    <div className="w-full h-full flex md:flex-row flex-col">
+    <div className="w-full h-full flex md:flex-row flex-col ">
       <div className="md:w-1/2 m-3 p-4">
           <WeddingSummary />
       </div>
-      {/* FORM EDIT/ADD */}
-      <div className="md:w-1/2 m-3 p-4">
-      <div class="w-full flex-1 p-8 order-1  text-gray-400 sm:w-96 lg:w-full lg:order-2 lg:mt-0">
-        <div class="mb-8 pb-8 flex items-center border-b border-gray-600">
-          <div class="ml-5 flex flex-col justify-center items-center">
-            <span class="block  text-2xl text-gray-700 font-extrabold text-center">Add or edit wedding information</span>
-          </div>
-        </div>
-        <form onSubmit={ onSubmit }>
-        <div className="block mb-4 border border-gray-200 rounded-lg">
-          <input type="text" onChange={ onChange } name='title' value={ wedding.title } className="block w-full px-4 py-3 border-2 border-transparent rounded-lg focus:border-red-300 focus:outline-none" placeholder="Title"/>
-        </div>
-        <div className="block mb-4 border border-gray-200 rounded-lg">
-          <input type="text" onChange={ onChange } name='address' value={ wedding.address } className="block w-full px-4 py-3 border-2 border-transparent rounded-lg focus:border-red-300 focus:outline-none" placeholder="Address"/>
-        </div>
-        <div className="block mb-4 border border-gray-200 rounded-lg">
-          <input type="date" onChange={ onChange } name='date' value={ wedding.date.slice(0,10) } className="block w-full px-4 py-3 border-2 border-transparent rounded-lg focus:border-red-300 focus:outline-none" placeholder="Date"/>
-        </div>
-        <div className="block mb-4 border border-gray-200 rounded-lg">
-          <input type="text" onChange={ onChange } name='groomName' value={ wedding.groomName } className="block w-full px-4 py-3 border-2 border-transparent rounded-lg focus:border-red-300 focus:outline-none" placeholder="Groom Name"/>
-        </div>
-        <div className="block mb-4 border border-gray-200 rounded-lg">
-          <input type="text" onChange={ onChange } name='brideName' value={ wedding.brideName } className="block w-full px-4 py-3 border-2 border-transparent rounded-lg focus:border-red-300 focus:outline-none" placeholder="Bride Name"/>
-        </div>
-        <div className="block mb-4 border border-gray-200 rounded-lg">
-            <input type='file' onChange={onChangeImg} name='brideImg' accept='file/*'  />
-            {
-              wedding.brideImg && (
-                <img 
-                src={wedding.brideImg}
-                alt='chosen'
-                style={{height:'300px'}}
-                />
-              )
-            }
-        </div>
-        <div className="block mb-4 border border-gray-200 rounded-lg">
-            <input type='file' onChange={onChangeImg} name='groomImg' accept='file/*' />
-            {
-              wedding.groomImg && (
-                <img 
-                src={wedding.groomImg}
-                alt='chosen'
-                style={{height:'300px'}}
-                />
-              )
-            }
-        </div>
-        <div className="block">
-          <button id='weddingBtn' className="w-full px-3 py-2 font-medium bg-red-400 text-white hover:bg-red-300 hover:text-gray-600 rounded-lg">SUBMIT</button>
-        </div>
-            
-        {/* <ImageUploader
-              withIcon={true}
-              buttonText='Choose images'
-              onChange={onDrop}
-              imgExtension={['.jpg', '.gif', '.png', '.gif']}
-              maxFileSize={5242880}
-          />
-          <img alt="" src={pictures[pictures.length - 1]}> */}
-
-          {/* </img> */}
-
-          {/* <ImageUploader
-              withIcon={true}
-              buttonText='Choose images'
-              onChange={onDropGroom}
-              imgExtension={['.jpg', '.gif', '.png', '.gif']}
-              maxFileSize={5242880}
-          />
-          <img alt="" src={picturesGroom[picturesGroom.length - 1]}></img> */}
-        </form>
-      </div>
-    </div>
+        {/* FORM EDIT/ADD */}
+      <div className="md:w-1/2 m-3 p-4 ">
+        {/*  */}
+        <div className="leading-loose">
+          <form className="max-w-xl m-4 p-10 bg-white rounded shadow-xl" onSubmit={ onSubmit }>
+            <p className="text-gray-800 font-bold">Edit Wedding Information</p>
+            <div className="mt-2">
+              <label className="block text-sm text-gray-600">Name of the Event</label>
+              <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+               type="text" onChange={ onChange } name='title' value={ wedding.title } placeholder="Pernikahan" />
+            </div>
+            <div className="mt-2">
+              <label className="block text-sm text-gray-600">Event Location</label>
+              <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+               type="text" onChange={ onChange } name='address' value={ wedding.address } placeholder="Gedung Perhutani, Pasar Minggu" />
+            </div>
+            <div className="mt-2">
+              <label className="block text-sm text-gray-600" >Event Date</label>
+              <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+               type="date" onChange={ onChange } name='date' value={ wedding.date.slice(0,10) } />
+            </div>
+            <div className="inline-block mt-2 w-1/2 pr-1">
+              <label className="block text-sm text-gray-600">Groom's Full Name</label>
+              <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+               type="text" onChange={ onChange } name='groomName' value={ wedding.groomName } placeholder="Semmi Verian Putra" />
+            </div>
+            <div className="inline-block mt-2 w-1/2 pr-1">
+              <label className="block text-sm text-gray-600">Bride's Full Name</label>
+              <input className="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded"
+               type="text" onChange={ onChange } name='brideName' value={ wedding.brideName } placeholder="Sophia Latjuba" />
+            </div>
+            <div className="inline-block mt-2 w-1/2 pr-1" >
+              <label className="block text-sm text-gray-600">Groom's Picture</label>
+              <input type='file' onChange={onChangeImg} name='groomImg' accept='file/*' 
+              className="text-gray-600 text-sm py-1 tracking-wider rounded "/>
+                {
+                  wedding.groomImg && (
+                    <img 
+                    src={wedding.groomImg}
+                    alt='chosen'
+                    style={{height: '100px'}}
+                    />
+                  )
+                }
+            </div>
+            <div className="inline-block mt-2 w-1/2 pr-1">
+              <label className="block text-sm text-gray-600">Bride's Picture</label>
+              <input type='file' onChange={onChangeImg} name='brideImg' accept='file/*' 
+              className="text-gray-600 text-sm py-1 tracking-wider rounded "/>
+                {
+                  wedding.brideImg && (
+                    <img 
+                    src={wedding.brideImg}
+                    alt='chosen'
+                    style={{height: '100px'}}
+                    />
+                  )
+                }
+            </div>
+            <div className="mt-4">
+              <button className="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded hover:bg-gray-800" type="submit">Submit Changes</button>
+            </div>
+          </form>
+        </div>{/* leading loose */} 
+      </div> {/* div nesting form */}
+      {/* div terluar */}
     </div>
   </>
     
--- src/components/WeddingSummary.jsx
+++ src/components/WeddingSummary.jsx
@@ -1,46 +1,107 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import moment from 'moment'
 
 const WeddingSummary = () => {
   const { wedding } = useSelector(state => state.wedding)
 
   return (
     <div class="flex flex-col justify-between items-center lg:flex-row lg:items-start">
-      <div class="w-full flex-1 p-8 order-1  text-gray-400 sm:w-96 lg:w-full lg:order-2 lg:mt-0">
+      <div className="w-full md:p-10 px-2 mt-5 bg-gray-800 rounded-lg shadow-lg text-gray-300 flex flex-col p-3">
+          <p className="font-bold text-2xl text-center text-gray-400 p-3 rounded shadow-lg bg-gray-700">WEDDING SUMMARY</p>
+          <div className="inline-block mt-2 pr-1 ">
+            <p className="text-xl text-gray-300 divide-opacity-25 border-b-2 border-white py-2">Event: <span className="font-bold">{wedding.title}</span></p>
+          </div>
+          <div className="inline-block mt-5 pr-1 flex flex-row">
+            <div className="flex md:w-1/2 w-full">
+              <svg className="w-6 h-6 mx-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" /></svg>
+              <p>{wedding.address}</p>
+            </div>
+            <div className="flex md:w-1/2 w-full">
+              <svg className="w-6 h-6 mx-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" /></svg>
+              <p>{moment(wedding.date).format("MMM Do YY")}</p>
+            </div>
+          </div>
+          <div className="inline-block mt-2 pr-1 flex flex-row">
+            <div className="flex md:w-1/2 w-full">
+              <div className="inline-block mt-2 pr-1 flex flex-col justify-center items-center">
+                {
+                  wedding && <img src={wedding.groomImg}  alt="" class="rounded-3xl mx-5 my-2" style={{ width: '120px', height: '120px'}}/>
+                }
+                <p>{wedding.groomName}</p>
+              </div>
+            </div>
+            <div className="flex md:w-1/2 w-full">
+              <div className="inline-block mt-2 pr-1 flex flex-col justify-center items-center">
+                {
+                  wedding && <img src={wedding.brideImg}  alt="" class="rounded-3xl mx-5 my-2" style={{ width: '120px', height: '120px'}} />
+                }
+                <p>{wedding.brideName}</p>
+              </div>
+            </div>
+          </div>
+      </div>
+      {/* <div class="w-full flex-1 p-8 order-1  text-gray-600 sm:w-96 lg:w-full lg:order-2 lg:mt-0">
         <div class="mb-8 pb-8 flex items-center border-b border-gray-600">
           <div class="ml-5 flex flex-col justify-center items-center">
             <span class="block  text-2xl text-gray-800 font-extrabold ">Wedding Information</span>
           </div>
         </div>
-        <ul class="mb-10 font-medium text-xl">
-        <li class="flex">
-            <span class="ml-3 mb-3">Event: <span className="text-gray-700">{wedding.title}</span></span>
-          </li>
-          <li class="flex">
-            <span class="ml-3 my-3">Date of the event: <span className="text-gray-700">{wedding.date.slice(0, 10)}</span></span>
-          </li>
-          <li class="flex">
-            <span class="ml-3 my-3">Location: <span className="text-gray-700">{wedding.address}</span></span>
-          </li>
-          <li class="flex">
-            <span class="ml-3 my-3">Groom's Full Name: <span className="text-gray-700">{wedding.groomName}</span></span>
-          </li>
-          <li class="flex">
-            <span class="ml-3 my-3">Bride's Full Name: <span className="text-gray-700">{wedding.brideName}</span></span>
-          </li>
-          <li class="flex flex-col">
-            <span class="ml-3 my-3">Bride & Groom's picture: </span>
-            <div className="flex w-full items-center">
+        <div class="mb-10 font-medium text-xl">
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p className="font-light text-black text-medium">Event</p>
+          </div>
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p>{wedding.title}</p>
+          </div>
+
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p className="font-light text-black text-medium">Event Location</p>
+          </div>
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p>{wedding.address}</p>
+          </div>
+
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p className="font-light text-black text-medium">Event Date</p>
+          </div>
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p>{moment(wedding.date).format("MMM Do YY")}</p>
+          </div>
+
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p className="font-light text-black text-medium">Groom's Full Name</p>
+          </div>
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p>{wedding.groomName}</p>
+          </div>
+          
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p className="font-light text-black">Bride's Full Name</p>
+          </div>
+          <div className="inline-block mt-2 w-1/2 pr-1">
+            <p>{wedding.brideName}</p>
+          </div>
+
+          <div className="inline-block mt-2 w-full pr-1">
+            <p className="font-light text-black">Bride & Groom's Picture</p>
+          </div>
+          <div className="inline-block mt-2 w-full pr-1 flex justify-center items-center">
+            <div className="inline-block mt-2 w-1/2 pr-1 flex flex-col justify-center items-center">
               {
                 wedding && <img src={wedding.groomImg}  alt="" class="rounded-3xl mx-5 my-2" style={{ width: '120px', height: '120px'}}/>
               }
+               <p>{wedding.groomName}</p>
+            </div>
+            <div className="inline-block mt-2 w-1/2 pr-1 flex flex-col justify-center items-center">
               {
                 wedding && <img src={wedding.brideImg}  alt="" class="rounded-3xl mx-5 my-2" style={{ width: '120px', height: '120px'}} />
               }
+               <p>{wedding.brideName}</p>
             </div>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </div>
+      </div> */}
     </div>
   )
 }
