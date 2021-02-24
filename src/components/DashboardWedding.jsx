89e4e29566b265b2248a9c29feb513fc1f416b2a
amandarzq <amanda.rzq@gmail.com>
2021-02-24 15:01:46 +0700

    cobacoba

--- src/components/DashboardWedding.jsx
+++ src/components/DashboardWedding.jsx
@@ -23,6 +23,11 @@ const DashboardWedding = () => {
       console.log(reader.result);
       dispatch(changeWeddingState(name, reader.result))
     }
+    if (realBtnBride.value){
+      customTxtBride.innerHTML = realBtnBride.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]
+    } else {
+      customTxtBride.innerHTML = "No file chosen, yet"
+    }
   }
 
   const onSubmit = async (e) => {
@@ -32,6 +37,26 @@ const DashboardWedding = () => {
     cancelBtnLoading('weddingBtn', 'SUBMIT')
   }
   
+  const realBtnBride = document.getElementById('realButtonBride')
+  const customBtnBride = document.getElementById('customButtonBride')
+  const customTxtBride = document.getElementById('customTextBride')
+
+  const triggerBrideBtn = () => {
+    customBtnBride.addEventListener("click", () => {
+    realBtnBride.click()
+  })
+  }
+  
+
+  // realBtnBride.addEventListener('change', () => {
+  //   if (realBtnBride.value){
+  //     customTxtBride.innerHTML = realBtnBride.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]
+  //   } else {
+  //     customTxtBride.innerHTML = "No file chosen, yet"
+  //   }
+  // })
+
+
   if(isLoading) return <h1>Loading..</h1>
   return (
     <>
@@ -84,9 +109,13 @@ const DashboardWedding = () => {
                 }
             </div>
             <div className="inline-block mt-2 w-1/2 pr-1">
-              <label className="block text-sm text-gray-600">Bride's Picture</label>
-              <input type='file' onChange={onChangeImg} name='brideImg' accept='file/*' 
-              className="text-gray-600 text-sm py-1 tracking-wider rounded "/>
+              <label className="block text-sm text-gray-600"
+              style={{}}>Bride's Picture
+              </label>
+              <input id="realButtonBride" type='file' onChange={onChangeImg} name='brideImg' accept='file/*' hidden="hidden"/>
+              <button type='button' id="customButtonBride"
+              onClick={triggerBrideBtn()}>Choose File</button>
+              <span id="customTextBride">No file chosen, yet.</span>
                 {
                   wedding.brideImg && (
                     <img 
